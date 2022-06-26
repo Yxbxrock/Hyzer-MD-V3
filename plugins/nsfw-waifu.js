1e24cb578637a83405526652d3972f3f34ca795f
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-06-26 19:08:09 +0700

    Create nsfw-waifu.js

---
+++ plugins/nsfw-waifu.js
@@ -0,0 +1,17 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => {
+  let res = await fetch('https://api.waifu.pics/nsfw/waifu')
+  if (!res.ok) throw 'Error Website sedang down'
+  let json = await res.json()
+  if (!json.url) throw 'Error!'
+  conn.sendFile(m.chat, json.url, '', 'sange kok ama kartun', m, 0, { thumbnail: Buffer.alloc(0) })
+}
+handler.help = ['hentai']
+handler.tags = ['anime']
+handler.command = /^(hentai)$/i
+
+handler.limit = true
+handler.nsfw = true
+handler.private = true
+
+module.exports = handler
