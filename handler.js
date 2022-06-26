32939de34ffe45f5f380de60adc0550ec0e02479
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-06-26 19:20:38 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -430,7 +430,7 @@ module.exports = {
                     isBanned: false,
                     welcome: true,
                     detect: false,
-                    sWelcome: '',
+                    sWelcome: 'Hai, Selamat datang di Group\n@subject\n\n@desc',
                     sBye: '',
                     sPromote: '',
                     sDemote: '',
@@ -708,7 +708,7 @@ module.exports = {
                         } finally {
                             text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Yah,si Beban Masuk Grup').replace('@subject', groupMetadata.subject).replace('@desc', groupMetadata.desc.toString()) :
                                 (chat.sBye || this.bye || conn.bye || 'Sip, Beban Berkurang 1'))
-                                this.sendButtonImg(id, pp, text, "Group Message", "Hi Beban 👋", "wkwk", null)
+                                this.sendButtonImg(id, pp, text, "Group Message", "Y", "wkwk", null)
                                 }
                     }
                 }
