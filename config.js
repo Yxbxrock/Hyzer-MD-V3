81b8ecbba15c7a82ba6c25d15d9be1c4143b7b2b
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-06-26 20:23:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,9 +13,9 @@ global.pulsa = '082365089294'
 global.gopay = '087892711054'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'karyl-bot'
-global.gc = 'https://chat.whatsapp.com/BYcW7rue6fA8gNdyLPedDe'
-global.web = 'https://hyzerr-apii.herokuapp.com' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
+global.namebot = 'Karyl Bot'
+global.gc = 'https://chat.whatsapp.com/BjLIOy9kDZ4HQJawOiX4mh'
+global.web = 'https://youtu.be/QB7ACr7pUuE' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
 
 //=======『 Tampilan Dan Lainnya 』============//
@@ -25,8 +25,8 @@ global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
 global.wm = 'karyl-bot'
 global.watermark = wm
-global.wm2 = '                     「 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳 あ⁩ 」'
-global.wm3 = '⫹⫺ 𝚂𝙷𝙸𝚁𝙾-𝙼𝙳'
+global.wm2 = 'Karyl Bot'
+global.wm3 = 'Karyl Bot'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/72168c6f1aaa90315c369.jpg'
