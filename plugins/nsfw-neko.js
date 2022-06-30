b44c993e7852e6e9f13f00bc0ddb56c5dfcba70a
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-06-30 16:15:46 +0700

    Update nsfw-neko.js

--- plugins/nsfw-neko.js
+++ plugins/nsfw-neko.js
@@ -4,14 +4,14 @@ let handler = async (m, { conn }) => {
   if (!res.ok) throw 'Error Website sedang down'
   let json = await res.json()
   if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'sange kok ama kucing', m, 0, { thumbnail: Buffer.alloc(0) })
+conn.sendButtonImg(m.chat, json.url, "sange kok ama kucing", "Karyl Bot", 'Next', ".nekonsfw", m)
 }
-handler.help = ['hentaineko']
+handler.help = ['nekonsfw']
 handler.tags = ['anime']
 handler.command = /^(hentaineko|nekonsfw)$/i
 
 handler.limit = true
 handler.nsfw = true
-handler.private = true
+handler.private = false
 
 module.exports = handler
