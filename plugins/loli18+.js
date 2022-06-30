c2c4e5fa83fa6bb8e095c2e8ee7de6678284a610
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-06-30 16:13:36 +0700

    Update loli18+.js

--- plugins/loli18+.js
+++ plugins/loli18+.js
@@ -1,13 +1,13 @@
 let handler = async (m, { conn }) => {
   conn.sendFile(m.chat, 'https://api.lolhuman.xyz/api/random/nsfw/loli?apikey=39f938655e624cb72a79560b', '', 'sange~an', m)
 }
-handler.help = ['loli18']
+handler.help = ['lolinsfw']
 handler.tags = ['anime']
-handler.command = /^(loli18)$/i
+handler.command = /^(lolinsfw)$/i
 
 handler.limit = true
 handler.group = false
 handler.nsfw = true
-handler.private = true
+handler.private = false
 
 module.exports = handler
