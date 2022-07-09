d1f219783b73c2e098a3a67f08132c46c774d90a
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-07-09 16:18:16 +0700

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -10,7 +10,7 @@ conn.sendFile(m.chat, helloarie, '', '', m, true)
 handler.customPrefix = /^(bot|robot)$/i
 handler.command = new RegExp
 
-handler.limit = true
+handler.limit = false
 handler.mods = false 
 handler.premium = false
 handler.group = false
