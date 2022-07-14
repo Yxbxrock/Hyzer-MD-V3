e86250ff3767ba7337dd2d290c1656c38bd64624
Yxbxrock <102041995+Yxbxrock@users.noreply.github.com>
2022-07-14 11:05:11 +0700

    Create haigabut.js

---
+++ plugins/haigabut.js
@@ -0,0 +1,19 @@
+let fs = require('fs')
+const { MessageType } = require('@adiwajshing/baileys')
+let handler = async (m, { conn }) => {
+let helloarie = fs.readFileSync('./media/mp3_haigabut.opus') 
+conn.sendFile(m.chat, helloarie, '', '', m, true)
+//conn.sendMessage(m.chat, helloarie, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
+// await conn.sendMessage(m.chat, { audio: { url: helloarie }, mimetype: 'audio/mp4'}, m)
+}
+
+handler.customPrefix = /^(hi|hai)$/i
+handler.command = new RegExp
+
+handler.limit = false
+handler.mods = false 
+handler.premium = false
+handler.group = false
+handler.private = false
+
+module.exports = handler
